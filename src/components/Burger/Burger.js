006918f19c8161da6e848dd1d4811541b5c49471
Daniel jimenez <djimenez017@gmail.com>
2019-02-28 22:35:18 -0800

    starting to build the dynamic ingredients on this hamburger builder.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,17 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 
 const burger = ( props ) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+                return [...Array(props.ingredients[igKey])].map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />;
+                });
+     });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="bacon" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" /> 
         </div>
     );
