4e60b5a9460ccaa7e685191839514f5729ff17cd
Daniel Jimenez <Daniel@CORP.EXACTSTAFF.COM>
2019-03-20 10:23:41 -0700

    added the .css extension to the classes

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import classes from './Backdrop';
+import classes from './Backdrop.css'; 
+//ADD THE .CSS EXTENSION
 
 const backdrop = (props) => (
     props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
